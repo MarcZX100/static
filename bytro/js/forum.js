d068c777082492de8b4de3e7bd9ef1fb31be84f3
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2024-05-13 23:42:01 +0200

    Update forum.js

--- bytro/js/forum.js
+++ bytro/js/forum.js
@@ -1,5 +1,6 @@
 function cambiarFoto() { // por alguna razón no va? con solo la linea de debajo?
   document.querySelectorAll(".contentHeaderIcon .userAvatarImage")[0].src = "https://i.ibb.co/dmDcDng/peter-griffin-popular-vibe.gif" <!-- avatar peter -->
+    console.log("Hola")
 }
 
 cambiarFoto()
