250b3510a5ee5a932533c3ecd9dc9741c8f1e8ce
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2024-05-13 23:41:15 +0200

    Update forum.js

--- bytro/js/forum.js
+++ bytro/js/forum.js
@@ -1,1 +1,5 @@
-document.querySelectorAll(".contentHeaderIcon .userAvatarImage")[0].src = "https://i.ibb.co/dmDcDng/peter-griffin-popular-vibe.gif" <!-- avatar peter -->
+function cambiarFoto() { // por alguna razón no va? con solo la linea de debajo?
+  document.querySelectorAll(".contentHeaderIcon .userAvatarImage")[0].src = "https://i.ibb.co/dmDcDng/peter-griffin-popular-vibe.gif" <!-- avatar peter -->
+}
+
+cambiarFoto()
